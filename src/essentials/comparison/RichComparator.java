d403402f6a6779cd8c386f09ae32005c8fc1fdeb
PatrickPlieschnegger <44707396+PatrickPlieschnegger@users.noreply.github.com>
2018-12-10 09:19:36 +0100

    Renamed "SimpleComparator" to "RichComparator"

--- src/essentials/comparison/RichComparator.java
+++ src/essentials/comparison/RichComparator.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
  * @author Patrick Plieschnegger
  * Purpose: A more readable Comparator which is compatible to the original java.lang interface.
  */
-public interface SimpleComparator<T> extends Comparator<T> {
+public interface RichComparator<T> extends Comparator<T> {
 
     default boolean smaller(T val1, T val2) {
         return compare(val1, val2) < 0;
