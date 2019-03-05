5e12eadb80d921ca09510f8b4ee8b122d1b57c4e
Patrick Plieschnegger <patrick.plieschnegger@massiveart.com>
2019-03-05 14:40:37 +0100

    added null initializer

---
+++ src/essentials/contract/Nulls.java
@@ -0,0 +1,17 @@
+package essentials.contract;
+
+import java.util.function.Supplier;
+
+/**
+ * @author Patrick Plieschnegger
+ */
+public final class Nulls {
+
+    private Nulls() {
+        throw new InstanceNotAllowedException(getClass());
+    }
+
+    public static <T> T init(T value, Supplier<T> initializer) {
+        return value != null ? value : initializer.get();
+    }
+}
